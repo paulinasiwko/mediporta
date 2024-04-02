1b4d2878d3fe5a1b19df8d9d2a8d7d2df2e3193e
Paulina Siwko <pau.siwko@gmail.com>
2024-04-02 15:36:15 +0200

    creted data with fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 import * as React from 'react';
@@ -24,32 +24,16 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
 
-function createData(id, name, calories, fat, carbs, protein) {
+function createData(id, name, count, synonyms, moderator, required) {
   return {
     id,
     name,
-    calories,
-    fat,
-    carbs,
-    protein,
+    count,
+    synonyms,
+    moderator,
+    required,
   };
 }
-
-const rows = [
-  createData(1, 'Cupcake', 305, 3.7, 67, 4.3),
-  createData(2, 'Donut', 452, 25.0, 51, 4.9),
-  createData(3, 'Eclair', 262, 16.0, 24, 6.0),
-  createData(4, 'Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData(5, 'Gingerbread', 356, 16.0, 49, 3.9),
-  createData(6, 'Honeycomb', 408, 3.2, 87, 6.5),
-  createData(7, 'Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData(8, 'Jelly Bean', 375, 0.0, 94, 0.0),
-  createData(9, 'KitKat', 518, 26.0, 65, 7.0),
-  createData(10, 'Lollipop', 392, 0.2, 98, 0.0),
-  createData(11, 'Marshmallow', 318, 0, 81, 2.0),
-  createData(12, 'Nougat', 360, 19.0, 9, 37.0),
-  createData(13, 'Oreo', 437, 18.0, 63, 4.0),
-];
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -88,31 +72,31 @@ const headCells = [
     id: 'name',
     numeric: false,
     disablePadding: true,
-    label: 'Dessert (100g serving)',
+    label: 'Name',
   },
   {
-    id: 'calories',
+    id: 'count',
     numeric: true,
     disablePadding: false,
-    label: 'Calories',
+    label: 'Count',
   },
   {
-    id: 'fat',
-    numeric: true,
+    id: 'synonyms',
+    numeric: false,
     disablePadding: false,
-    label: 'Fat (g)',
+    label: 'Has synonyms',
   },
   {
-    id: 'carbs',
-    numeric: true,
+    id: 'moderator',
+    numeric: false,
     disablePadding: false,
-    label: 'Carbs (g)',
+    label: 'Is moderator only',
   },
   {
-    id: 'protein',
-    numeric: true,
+    id: 'required',
+    numeric: false,
     disablePadding: false,
-    label: 'Protein (g)',
+    label: 'Is required',
   },
 ];
 
@@ -129,7 +113,7 @@ function EnhancedTableHead(props) {
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
+            align='right'
             padding={headCell.disablePadding ? 'none' : 'normal'}
             sortDirection={orderBy === headCell.id ? order : false}
           >
@@ -175,16 +159,6 @@ function EnhancedTableToolbar(props) {
         }),
       }}
     >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
         <Typography
           sx={{ flex: '1 1 100%' }}
           variant="h6"
@@ -193,21 +167,6 @@ function EnhancedTableToolbar(props) {
         >
           Nutrition
         </Typography>
-      )}
-
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
     </Toolbar>
   );
 }
@@ -217,12 +176,28 @@ EnhancedTableToolbar.propTypes = {
 };
 
 export default function EnhancedTable() {
+  const [rows, setRows] = useState([]);
   const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [orderBy, setOrderBy] = React.useState('count');
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const URL = 'https://api.stackexchange.com/2.3/tags?site=stackoverflow&order=desc&sort=popular&filter=default';
+      try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        const newRows = data.items.map((element, index) => createData(index + 1, element.name, element.count, element.has_synonyms, element.is_moderator_only, element.is_required));
+        setRows(newRows);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
@@ -334,13 +309,14 @@ export default function EnhancedTable() {
                       id={labelId}
                       scope="row"
                       padding="none"
+                      align="right"
                     >
                       {row.name}
                     </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell align="right">{row.count}</TableCell>
+                    <TableCell align="right">{row.synonyms ? "Yes" : "No"}</TableCell>
+                    <TableCell align="right">{row.moderator ? "Yes" : "No"}</TableCell>
+                    <TableCell align="right">{row.required ? "Yes" : "No"}</TableCell>
                   </TableRow>
                 );
               })}

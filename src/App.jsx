ef88d7037ffc500b293768fe78f32eba56e1dace
Paulina Siwko <pau.siwko@gmail.com>
2024-04-02 22:34:01 +0200

    adding isLoading

--- src/App.jsx
+++ src/App.jsx
@@ -2,307 +2,106 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import { visuallyHidden } from '@mui/utils';
+import { DataGrid } from '@mui/x-data-grid';
+import axios from 'axios'; // Import axios for making HTTP requests
 
-function createData(id, name, count) {
-  return {
-    id,
-    name,
-    count,
-  };
-}
 
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-  {
-    id: 'name',
-    numeric: false,
-    disablePadding: true,
-    label: 'Name',
-  },
-  {
-    id: 'count',
-    numeric: true,
-    disablePadding: false,
-    label: 'Count',
-  },
-];
-
-function EnhancedTableHead(props) {
-  const { order, orderBy, onRequestSort } =
-    props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align='center'
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
-function EnhancedTableToolbar(props) {
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-        }),
-      }}
-    >
-        <Typography
-          sx={{ flex: '1 1 100%', fontWeight: 600 }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Tags
-        </Typography>
-    </Toolbar>
-  );
-}
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
-
-export default function EnhancedTable() {
+export default function DataTable() {
   const [rows, setRows] = useState([]);
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('count');
-  const [selected, setSelected] = React.useState([]);
-  const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [selectedPageSize, setSelectedPageSize] = useState(25); 
+  const [selectedOrder, setSelectedOrder] = useState('');
+  const [selectedSort, setSelectedSort] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const columns = [
+    { field: 'name', headerName: 'Name'},
+    { field: 'count', headerName: 'Count' },
+  ];
+
+  const fetchNextPageData = (currentPage, currentPageSize) => {
+    // Fetch additional data for the next page from your API
+    setTableState((prev) => {return {rows: prev.rows, page: currentPage, isLoading: true}})
+    setPage(p => currentPage);
+    setSelectedPageSize(p => currentPageSize);
+    console.log(currentPage)
+    setIsLoading(prevLoading => true);
+
+    axios.get(`https://api.stackexchange.com/2.3/tags?key=L44lhuKUbnH4H4FN4hrY6g((&site=stackoverflow&page=${currentPage}&pagesize=${currentPageSize}&order=${selectedOrder}&sort=${selectedSort}`)
+      .then(response => {
+        console.log(response)
+        const newRows = response.data.items.map((element, index) => {
+          return {id: index, name: element.name, count: element.count}
+        })
+        setTableState((prev) => {return {rows: newRows, page: prev.page + 1, isLoading: false}})
+        setRows(p => newRows); // Append new data to the existing rows
+        setPage(p => page + 1); // Update the page number
+        setIsLoading(prevLoading => false);
+
+      })
+      .catch(error => {
+        console.error('Error fetching next page data:', error);
+        setIsLoading(prevLoading => false);
+      });
+  };
+
+  const handlePageChange = (params) => {
+    fetchNextPageData(params.page + 1, params.pageSize);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const URL = 'https://api.stackexchange.com/2.3/tags?site=stackoverflow&order=desc&sort=popular&filter=default';
-      try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        const newRows = data.items.map((element, index) => createData(index + 1, element.name, element.count));
-        setRows(newRows);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchData();
-  }, []);
+    // Fetch initial data when component mounts
+    fetchNextPageData(1, selectedPageSize);
+  }, [selectedOrder, selectedSort]);
 
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
+  useEffect(() => {
+    const totalURL = `https://api.stackexchange.com/2.3/tags?key=L44lhuKUbnH4H4FN4hrY6g((&site=stackoverflow&order=desc&sort=popular&filter=total`;
+    axios.get(totalURL)
+      .then(response => {
+        setTotal(response.data.total);
+      })
+      .then(() => {
+        setIsLoading(prevLoading => false);
 
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n) => n.id);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
+      })
+  }, [])
 
-  const handleClick = (event, id) => {
-    const selectedIndex = selected.indexOf(id);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, id);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-    setSelected(newSelected);
-  };
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleChangeDense = (event) => {
-    setDense(event.target.checked);
-  };
-
-  const isSelected = (id) => selected.indexOf(id) !== -1;
-
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
-  const visibleRows = React.useMemo(
-    () =>
-      stableSort(rows, getComparator(order, orderBy)).slice(
-        page * rowsPerPage,
-        page * rowsPerPage + rowsPerPage,
-      ),
-    [order, orderBy, page, rowsPerPage],
-  );
+  const handleOrderChange = (event) => {
+    setSelectedOrder(event.target.value);
+  }
+  const handleSortChange = (event) => {
+    setSelectedSort(event.target.value);
+  }
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-        sx={{backgroundColor: '#B7E0A6', marginTop: '40px', borderRadius: '5px 5px 0 0'}}
-      />
-      <Paper sx={{ width: '100%', mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-            size={dense ? 'small' : 'medium'}
-          >
-            <EnhancedTableHead
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {visibleRows.map((row, index) => {
-                const isItemSelected = isSelected(row.id);
-                const labelId = `enhanced-table-checkbox-${index}`;
-
-                return (
-                  <TableRow
-                    hover
-                    onClick={(event) => handleClick(event, row.id)}
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={row.id}
-                    selected={isItemSelected}
-                    sx={{ cursor: 'pointer' }}
-                  >
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
-                      align="center"
-                    >
-                      {row.name}
-                    </TableCell>
-                    <TableCell align="center">{row.count}</TableCell>
-                  </TableRow>
-                );
-              })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: (dense ? 33 : 53) * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-      <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      />
-    </Box>
+    <div style={{ height: 400, width: '100%' }}>
+      <select value={selectedOrder} onChange={handleOrderChange}>
+        <option value=''>Order</option>
+        <option value='desc'>desc</option>
+        <option value='asc'>asc</option>
+      </select>
+      <select value={selectedSort} onChange={handleSortChange}>
+        <option value=''>Sort</option>
+        <option value='popular'>popular</option>
+        <option value='name'>name</option>
+      </select>
+      {!isLoading ?       
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={selectedPageSize}
+        pageSizeOptions={[5, 10, 25]}
+        initialState={{pagination: {paginationModel: { pageSize: 25, page: 0 }}}}
+        rowCount={total}
+        paginationMode='server'
+        onPaginationModelChange={handlePageChange}
+      /> :
+      <p>Ładowanie</p>
+}
+    </div>
   );
 }
+
+
+
